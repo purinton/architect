5c0ec23ae2f02b51d0ce9c92a7ba3734efa71835
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-25 07:15:13 -0400

    Update ready.mjs

--- events/ready.mjs
+++ events/ready.mjs
@@ -3,4 +3,12 @@ export default async function ({ log, presence }, client) {
     log.debug('ready', { tag: client.user.tag });
     log.info(`Logged in as ${client.user.tag}`);
     if (presence) client.user.setPresence(presence);
+    for (const guild of client.guilds.cache.values()) {
+        try {
+            await guild.members.fetch();
+            log.info(`Fetched ${guild.memberCount} members of ${guild.name}`);
+        } catch (err) {
+            log.error(`Failed to fetch members for guild: ${guild.name}`, err);
+        }
+    }
 }
